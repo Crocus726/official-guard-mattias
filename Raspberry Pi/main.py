c73a3c1aeaf2bf98595b1464820b6e18df3105a3
Crocus726 <crocus726@gmail.com>
2023-07-05 15:53:37 +0900

    Update main.py

--- Raspberry Pi/main.py
+++ Raspberry Pi/main.py
@@ -1,2 +1,10 @@
+# TEST CODE, STILL WRITING
+
 import time
 import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+# GPIO.setup(channel, GPIO.IN)
+# GPIO.setup(channel, GPIO.OUT, initial = GPIO.LOW)
